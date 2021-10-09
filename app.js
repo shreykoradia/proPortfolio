352bcae4eddc8c248939c2462badc74e7db1934a
shreykoradia <shrey.koradia@yahoo.com>
2021-10-09 12:08:29 +0530

    done with the gsap animation

--- app.js
+++ app.js
@@ -11,10 +11,27 @@ let tl2 = gsap.timeline({
     scrollTrigger:{
         trigger:'.home',
         start:'0%',
+        end:'65%',
+        scrub:1
+    },
+});
+
+let tl4 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.home',
+        start:'0%',
         end:'80%',
         scrub:1
     },
 });
+
+tl.fromTo(".sliding-text",{y:0} , {y:-400});
+tl2.fromTo('.logo', {scale : 6 }, {scale : 1 , top : '2rem' , left : '3rem' , x : "50%" , y : "50%"  }
+);
+tl4.fromTo('.square',{left :"65%"} , {left : "100%" , opacity: 0 , rotation:90});
+
+
+
 
 let tl3 = gsap.timeline({
     scrollTrigger:{
@@ -26,13 +43,3 @@ let tl3 = gsap.timeline({
         pinSpacing:false
     },
 });
-
-
-
-
-
-
-tl.fromTo(".sliding-text",{y:0} , {y:-400});
-tl2.fromTo('.logo', {scale : 6 }, {scale : 1 , top : '2rem' , left : '3rem' , x : "50%" , y : "50%"  }
-);
-
